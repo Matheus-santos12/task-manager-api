dfff6cbfdd9c5d14dd92a9f1251647d651b87caf
Matheus Silva <rematheussilva@gmail.com>
2026-02-12 23:23:34 -0500

    redireciona o usuário para o dashboard.html

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,6 @@
+import { fetchTasks } from "./tasks.js";
+
+console.log(" O arquivo está sendo carregado");
 const logoutButton = document.getElementById("logout");
 
 logoutButton.addEventListener("click", (event) => {
@@ -9,8 +12,19 @@ logoutButton.addEventListener("click", (event) => {
 if (!window.localStorage.getItem("user")) {
   window.location.href = "index.html";
 } else {
+  console.log(" O usuário está logado");
   const user = JSON.parse(window.localStorage.getItem("user"));
   const welcomeMessage = document.getElementById("welcome-message");
   welcomeMessage.textContent = `Seja bem-vindo, ${user.name}!`;
+  window.location.href = "dashboard.html";
   console.log(user);
 }
+
+async function init() {
+  console.log("Iniciando a página🔴");
+  const tasks = await fetchTasks();
+  console.log(tasks);
+}
+console.log(" Chama a função init🔴");
+init();
+console.log(" Chamou a função init🔴");
