8be0f277ecc627b50fe0cf2a5455c6e8fa648ccd
Matheus Silva <rematheussilva@gmail.com>
2026-02-18 22:10:25 -0500

    Script para deletar de forma suave

--- js/tasks.js
+++ js/tasks.js
@@ -1,4 +1,4 @@
-import { del, get, patch, post } from "./api.js";
+import { get, patch, post } from "./api.js";
 
 const user = JSON.parse(window.localStorage.getItem("user"));
 
@@ -9,25 +9,33 @@ async function fetchTasks() {
   return tasks;
 }
 
-async function createTask(title, description) {
+async function createTask(title, description, priority, dueDate) {
   const newTask = {
-    userId: userId,
-    title: title,
-    description: description,
-    completed: false,
+    userId: String(user.id),
+    title,
+    description,
+    status: "pending",
+    priority,
+    dueDate,
+    deleted: false,
+    deletedAt: null,
     createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
   };
 
   return await post("/tasks", newTask);
 }
 
 async function deleteTask(taskId) {
-  const res = await del(`/tasks/${taskId}`);
-  return res;
+  const softDelete = await patch(`/tasks/${taskId}`, {
+    deleted: true,
+    deletedAt: new Date().toISOString(),
+  });
+  return softDelete;
 }
 
 async function toggleTask(taskId, completed) {
-  const newPatch = await patch(`/tasks/${taskId}`, { completed: !completed });
+  const newPatch = await patch(`/tasks/${taskId}`, { completed });
   return newPatch;
 }
 
